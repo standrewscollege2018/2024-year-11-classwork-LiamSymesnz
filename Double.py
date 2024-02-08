4dbb79fa5b5c33c50deb6f2e49cca93152e2a8e5
LiamSymesnz <159396726+LiamSymesnz@users.noreply.github.com>
2024-02-09 10:13:57 +1300

    Made it better

--- Double.py
+++ Double.py
@@ -1,15 +1,20 @@
 '''This programme takes a number and doubles it'''
 
-
+# Start a loop for error catching
 keep_asking = True
 while keep_asking:
+    # Takes input then doubles it if it is a number
     try:
-        number = float(input())
-        keep_asking = False
+        number = float(input("Enter a positive number"))
+        if number  >= 0:
+            keep_asking = False
+        else:
+            print("Hey dumbass, thats not a positive number!")
     except ValueError:
-        print("Hey dumbass, thats not a number")
+        print(f"Hey dumbass, thats not a number")
 
-print(number * 2)
+# Print the result
+print (f"{number} doubled is {number * 2}")
 
 
 
